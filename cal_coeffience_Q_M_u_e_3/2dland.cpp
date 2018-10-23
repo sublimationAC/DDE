acf237ce6b60d4c5ae1f0335bb78cd59d9026b9f
Wei Liu <lwwd1996@163.com>
2018-10-23 16:06:23 +0800

    Add files via upload

--- cal_coeffience_Q_M_u_e_3/2dland.cpp
+++ cal_coeffience_Q_M_u_e_3/2dland.cpp
@@ -1,5 +1,5 @@
 #include "2dland.h"
-#define flap_2dland
+//#define flap_2dland
 
 #define cal_land_num
 void load_img_land(std::string path, std::string sfx, iden *ide, int &id_idx, std::vector< std::vector<cv::Mat_<uchar> > > &imgs) {
@@ -26,7 +26,41 @@ void load_img_land(std::string path, std::string sfx, iden *ide, int &id_idx, st
 #endif // win64
 
 #ifdef linux
-	DIR *dir;
+	struct dirent **namelist;
+	int n;
+	n = scandir(path.c_str(), &namelist, 0, alphasort);
+	if (n < 0)
+	{
+		std :: cout << "scandir return " << n << "\n";
+		perror("Cannot open .");
+		exit(1);
+	}
+	else
+	{
+		int index = 0;
+		struct dirent *dp;
+		while (index < n)
+		{
+			dp = namelist[index];
+			std::cout << dp->d_name << ' ' << strlen(dp->d_name) << "\n";
+			if (dp->d_name[0] == '.') {
+				free(namelist[index]);
+				index++;
+				continue;
+			}
+			if (dp->d_type == DT_DIR) {
+				std::vector<cv::Mat_<uchar> > img_temp;
+				printf("Loading identity %d...\n", id_idx);
+				id_idx += load_img_land_same_id(path + "/" + dp->d_name, sfx, ide, id_idx, img_temp);
+				imgs.push_back(img_temp);
+			}
+			free(namelist[index]);
+			index++;
+		}
+		free(namelist);
+	}
+
+	/*DIR *dir;
 	struct dirent *dp;
 	if ((dir = opendir(path.c_str())) == NULL) {
 		perror("Cannot open .");
@@ -42,7 +76,7 @@ void load_img_land(std::string path, std::string sfx, iden *ide, int &id_idx, st
 			imgs.push_back(img_temp);
 		}
 	}
-	closedir(dir);
+	closedir(dir);*/
 #endif // linux	
 }
 
@@ -89,36 +123,86 @@ int load_img_land_same_id(std::string path, std::string sfx, iden *ide, int id_i
 
 #ifdef linux
 	int flag = 0;
-	DIR *dir;
-	struct dirent *dp;
-	if ((dir = opendir(path.c_str())) == NULL) {
+	struct dirent **namelist;
+	int n;
+	n = scandir(path.c_str(), &namelist, 0, alphasort);
+	if (n < 0)
+	{
+		std::cout << "scandir return " << n << "\n";
 		perror("Cannot open .");
 		exit(1);
+		
 	}
-	while ((dp = readdir(dir)) != NULL) {
-		//std::cout << dp->d_name << ' ' << dp->d_namlen << "\n";
-		if (dp->d_name[0] == '.') continue;
-		if (dp->d_type == DT_DIR)
-			flag |= load_img_land_same_id(path + "/" + dp->d_name, sfx, ide, id_idx, img_temp);
-		else {
-			int len =strlen( dp->d_name);
-			if (dp->d_name[len - 1] == 'd' && dp->d_name[len - 2] == 'n') {
-				////	
-				flag = 1;
-				load_land(path + "/" + dp->d_name, ide, id_idx);
-				std::string p = path + "/" + dp->d_name;
-				cv::Mat_<uchar> temp;
-				load_img(p.substr(0, p.find(".land")) + sfx, temp);
-				img_temp.push_back(temp);
-#ifdef  flap_2dland
-				for (int i = G_land_num * (ide[id_idx].num); i < G_land_num*(ide[id_idx].num + 1); i++)
-					ide[id_idx].land_2d(i, 1) = temp.rows - ide[id_idx].land_2d(i, 1);
-#endif //  flap_2dland
-				ide[id_idx].num++;
+	else
+	{
+		int index = 0;
+		struct dirent *dp;
+		while (index < n)
+		{
+			dp = namelist[index];
+			if (dp->d_name[0] == '.') {
+				free(namelist[index]);
+				index++;
+				continue;
 			}
+			if (dp->d_type == DT_DIR)
+				flag |= load_img_land_same_id(path + "/" + dp->d_name, sfx, ide, id_idx, img_temp);
+			else {
+				int len = strlen(dp->d_name);
+				if (dp->d_name[len - 1] == 'd' && dp->d_name[len - 2] == 'n') {
+					////	
+					flag = 1;
+					load_land(path + "/" + dp->d_name, ide, id_idx);
+					std::string p = path + "/" + dp->d_name;
+					cv::Mat_<uchar> temp;
+					load_img(p.substr(0, p.find(".land")) + sfx, temp);
+					img_temp.push_back(temp);
+#ifdef  flap_2dland
+					for (int i = G_land_num * (ide[id_idx].num); i < G_land_num*(ide[id_idx].num + 1); i++)
+						ide[id_idx].land_2d(i, 1) = temp.rows - ide[id_idx].land_2d(i, 1);
+#endif //  flap_2dland
+					ide[id_idx].num++;
+				}
+			}
+			free(namelist[index]);
+			index++;
+
 		}
+		free(namelist);
 	}
-	closedir(dir);
+
+//	DIR *dir;
+//	struct dirent *dp;
+//	if ((dir = opendir(path.c_str())) == NULL) {
+//		perror("Cannot open .");
+//		exit(1);
+//	}
+//	while ((dp = readdir(dir)) != NULL) {
+//		//std::cout << dp->d_name << ' ' << dp->d_namlen << "\n";
+//		if (dp->d_name[0] == '.') continue;
+//		if (dp->d_type == DT_DIR)
+//			flag |= load_img_land_same_id(path + "/" + dp->d_name, sfx, ide, id_idx, img_temp);
+//		else {
+//			int len =strlen( dp->d_name);
+//			if (dp->d_name[len - 1] == 'd' && dp->d_name[len - 2] == 'n') {
+//				////	
+//				flag = 1;
+//				load_land(path + "/" + dp->d_name, ide, id_idx);
+//				std::string p = path + "/" + dp->d_name;
+//				cv::Mat_<uchar> temp;
+//				load_img(p.substr(0, p.find(".land")) + sfx, temp);
+//				img_temp.push_back(temp);
+//#ifdef  flap_2dland
+//				for (int i = G_land_num * (ide[id_idx].num); i < G_land_num*(ide[id_idx].num + 1); i++)
+//					ide[id_idx].land_2d(i, 1) = temp.rows - ide[id_idx].land_2d(i, 1);
+//#endif //  flap_2dland
+//				ide[id_idx].num++;
+//			}
+//		}
+//	}
+//	closedir(dir);
+
+
 	return flag;
 #endif // linux
 }
@@ -186,7 +270,7 @@ void test_data_2dland(
 	cv::imshow("test_image", imgs[id_idx][img_idx]);
 	cv::waitKey(0);
 	/*for (int j = 0; j < landmark_num; j++)
-	printf("%.10f %.10f\n",ground_truth_shapes[1][j][0], ground_truth_shapes[1][j][1]);*/
+	printf("%.6f %.6f\n",ground_truth_shapes[1][j][0], ground_truth_shapes[1][j][1]);*/
 }
 
 void test_2dslt(
@@ -196,9 +280,9 @@ void test_2dslt(
 	//int mi = img_idx * G_land_num + 70;
 	CvFont font;
 	cvInitFont(&font, CV_FONT_HERSHEY_COMPLEX, 0.01, 0.5, 1, 0.5, 8);
-	IplImage *pImage = cvLoadImage("D:\\sydney\\first\\data_me\\test\\Tester_1\\TrainingPose/pose_22.jpg");
+	IplImage *pImage = cvLoadImage("D:\\sydney\\first\\data_me\\FaceWarehouse\\Tester_6\\TrainingPose/pose_17.jpg");
 	std::string si;
-	for (int i = img_idx * G_land_num + 27; i < img_idx * G_land_num + 35; i++) {
+	for (int i = img_idx * G_land_num + 59; i < img_idx * G_land_num + 65; i++) {
 		//27-35 66-74
 		si = std::to_string(i - img_idx * G_land_num), cvPutText(pImage, si.c_str(), cv::Point2f(ide[id_idx].land_2d(i, 0), ide[id_idx].land_2d(i, 1))
 			, &font, cv::Scalar(255, 255, 255));
@@ -234,7 +318,7 @@ void test_2dslt(
 	cv::waitKey(0);
 	fclose(fp);
 	/*for (int j = 0; j < landmark_num; j++)
-	printf("%.10f %.10f\n",ground_truth_shapes[1][j][0], ground_truth_shapes[1][j][1]);*/
+	printf("%.6f %.6f\n",ground_truth_shapes[1][j][0], ground_truth_shapes[1][j][1]);*/
 }
 
 void load_inner_land_corr(Eigen::VectorXi &cor) {
@@ -297,4 +381,84 @@ void test_3d22dland(cv::Mat_<uchar> img, std::string path,iden *ide,int id_idx,i
 		cv::waitKey(0);
 	}
 	fclose(fp);
+}
+
+void save_result(iden *ide, int tot_id, std ::string name) {
+	std ::cout << "saving coefficients...file:" << name << "\n";
+	FILE *fp;
+	fopen_s(&fp, name.c_str(), "wb");
+
+	fwrite(&tot_id, sizeof(int), 1, fp);
+	for (int i_id = 0; i_id < tot_id; i_id++) {
+		fwrite(&ide[i_id].num, sizeof(int), 1, fp);
+		for (int j = 0; j < G_iden_num; j++)
+			fwrite(&ide[i_id].user(j), sizeof(float), 1, fp);
+		for (int exp_idx = 0; exp_idx < ide[i_id].num; exp_idx++) {
+			for (int i_v = 0; i_v < G_land_num; i_v++) {
+				fwrite(&ide[i_id].land_2d(exp_idx*G_land_num + i_v, 0), sizeof(float), 1, fp);
+				fwrite(&ide[i_id].land_2d(exp_idx*G_land_num + i_v, 1), sizeof(float), 1, fp);
+			}
+
+			fwrite(&ide[i_id].center(exp_idx, 0), sizeof(float), 1, fp);
+			fwrite(&ide[i_id].center(exp_idx, 1), sizeof(float), 1, fp);
+
+			for(int i_shape=0;i_shape<G_nShape;i_shape++)
+				fwrite(&ide[i_id].exp(exp_idx,i_shape), sizeof(float), 1, fp);
+
+			for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++)
+				fwrite(&ide[i_id].rot(exp_idx * 3 + i, j), sizeof(float), 1, fp);
+
+			for (int i=0;i<3;i++) fwrite(&ide[i_id].tslt(exp_idx,i), sizeof(float), 1, fp);
+
+			for (int i_v = 0; i_v < G_land_num; i_v++) fwrite(&ide[i_id].land_cor(exp_idx, i_v), sizeof(int), 1, fp);
+
+			for (int i = 0; i < 2; i++) for (int j = 0; j < 3; j++)
+				fwrite(&ide[i_id].s(exp_idx * 2 + i, j), sizeof(float), 1, fp);
+
+			for (int i_v = 0; i_v < G_land_num; i_v++) {
+				fwrite(&ide[i_id].dis(exp_idx*G_land_num + i_v, 0), sizeof(float), 1, fp);
+				fwrite(&ide[i_id].dis(exp_idx*G_land_num + i_v, 1), sizeof(float), 1, fp);
+			}
+		}
+	}
+	fclose(fp);
+
+	puts("save successful!");
+}
+
+float cal_3d_vtx_(
+	iden *ide, Eigen::MatrixXf &bldshps,
+	int id_idx, int exp_idx, int vtx_idx, int axis) {
+
+	//puts("calculating one vertex coordinate...");
+	float ans = 0;
+
+	for (int i_id = 0; i_id < G_iden_num; i_id++)
+		for (int i_shape = 0; i_shape < G_nShape; i_shape++)
+			if (i_shape == 0)
+				ans += ide[id_idx].exp(exp_idx, i_shape)*ide[id_idx].user(i_id)
+				*bldshps(i_id, vtx_idx * 3 + axis);
+			else
+				ans += ide[id_idx].exp(exp_idx, i_shape)*ide[id_idx].user(i_id)
+				*(bldshps(i_id, 3 * G_nVerts*i_shape + vtx_idx * 3 + axis) - bldshps(i_id, vtx_idx * 3 + axis));
+	return ans;
+}
+
+void cal_dis(iden *ide, Eigen::MatrixXf &bldshps, int id_tot) {
+	puts("calculating displacement...");
+	for (int i_id = 0; i_id < id_tot; i_id++) {
+		Eigen::MatrixX2f land(G_land_num*ide[i_id].num,2);
+		for (int exp_idx = 0; exp_idx < ide[i_id].num; exp_idx++) {
+			Eigen::Matrix3f R = ide[i_id].rot.block(exp_idx * 3, 0, 3, 3);
+			Eigen::MatrixX3f s = ide[i_id].s.block(exp_idx * 2, 0, 2, 3);
+			Eigen::RowVector2f T = ide[i_id].tslt.block(exp_idx, 0, 1, 2);
+			for (int i_v = 0; i_v < G_land_num; i_v++) {
+				Eigen::Vector3f v;
+				for (int axis = 0; axis < 3; axis++)
+					v(axis) = cal_3d_vtx_(ide, bldshps, i_id, exp_idx, ide[i_id].land_cor(exp_idx, i_v), axis);
+				land.row(exp_idx*G_land_num + i_v) = (s*R*v).transpose() + T;
+			}
+		}
+		ide[i_id].dis.array() = ide[i_id].land_2d.array() - land.array();
+	}
 }
